a0fa8c69cb30846656b24d6b1785641e7583373e
hadi <elhadisaeed@gmail.com>
2022-10-01 09:47:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome ELHADIand Family!"
+    return "I think I am making good progress !"
 
 @app.route('/how are you')
 def hello():
